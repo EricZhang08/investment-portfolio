03845b328ed3260c1a352f5fbe763989c7f3a34d
EricZhang08 <jiankunzhang08@gmail.com>
2021-07-15 14:43:27 -0400

    created stock and user models

--- portfolio/main/admin.py
+++ portfolio/main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Stock, User
 
 # Register your models here.
+
+class MeetupAdmin(admin.ModelAdmin):
+  list_display = ('name', 'email')
+  list_filter = ('name',)
+
+admin.site.register(User, MeetupAdmin)
+admin.site.register(Stock)
--- portfolio/main/models.py
+++ portfolio/main/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django import forms
 
 # Create your models here.
+
+class Stock(models.Model):
+    ticker = models.CharField(max_length=30)
+    percentage = models.FloatField()
+
+    def __str__(self):
+        return self.ticker
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(unique=True)
+    password = forms.CharField(max_length=32, widget=forms.PasswordInput)
+    portfolio = models.ManyToManyField(Stock, blank=True, null=True)
+    def __str__(self):
+        return f'{self.name} - {self.email})'
+
+
+
+
+
+
